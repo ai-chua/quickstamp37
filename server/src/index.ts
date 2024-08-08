dcfbd93c0b684d7f1445f15c754b0c77a618d939
Ailin Chua <ai-chua@outlook.com>
2024-08-08 16:56:14 +0800

    basic ws set up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,33 @@
+import { createServer } from 'http'
+
 import { Application } from 'express'
+import io, { Server } from 'socket.io'
 
 import { PORT } from './consts'
 import { appFactory } from './utils/app'
 
 const app: Application = appFactory()
+const server = createServer(app)
+const ws = new Server(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST']
+	}
+})
 
-app.listen(PORT, () => {
-	console.log(`Snake server up on port ${PORT}`)
+ws.on('connection', (socket) => {
+	console.log('a user connected')
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected')
+	})
+
+	socket.on('message', (message) => {
+		console.log('message received: ', message)
+		socket.broadcast.emit('message', message)
+	})
+})
+
+server.listen(4000, () => {
+	console.log(`Server is running on http://localhost:${4000}`)
 })
